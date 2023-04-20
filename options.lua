bea41cb7a95c3e90d0f6c472b51ac314ed235bc7
sanderdrummer <tobias.pickel@moia.io>
2023-04-20 17:40:30 +0200

    feat: spell true

--- options.lua
+++ options.lua
@@ -4,8 +4,9 @@ return {
     -- set to true or false etc.
     relativenumber = true, -- sets vim.opt.relativenumber
     number = true, -- sets vim.opt.number
-    spell = false, -- sets vim.opt.spell
-    signcolumn = "auto", -- sets vim.opt.signcolumn to auto
+    spell = true, -- sets vim.opt.spell
+    spelllang = "en_us",
+    signcolumn = "auto", -- sets vim.opt.signColumn to auto
     wrap = false, -- sets vim.opt.wrap
   },
   g = {
@@ -18,12 +19,3 @@ return {
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
   },
 }
--- If you need more control, you can use the function()...end notation
--- return function(local_vim)
---   local_vim.opt.relativenumber = true
---   local_vim.g.mapleader = " "
---   local_vim.opt.whichwrap = vim.opt.whichwrap - { 'b', 's' } -- removing option from list
---   local_vim.opt.shortmess = vim.opt.shortmess + { I = true } -- add to option list
---
---   return local_vim
--- end
