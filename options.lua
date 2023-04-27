9b21635a2ccdca95a52a354116a106e58baae088
sanderdrummer <tobias.pickel@moia.io>
2023-04-27 14:26:22 +0200

    feat: not needed if null-ls is checking

--- options.lua
+++ options.lua
@@ -4,7 +4,7 @@ return {
     -- set to true or false etc.
     relativenumber = true, -- sets vim.opt.relativenumber
     number = true, -- sets vim.opt.number
-    spell = true, -- sets vim.opt.spell
+    spell = false, -- sets vim.opt.spell
     spelllang = "en_us",
     signcolumn = "auto", -- sets vim.opt.signColumn to auto
     wrap = false, -- sets vim.opt.wrap
