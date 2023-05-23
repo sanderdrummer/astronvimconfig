e7438baf3c2886f490d567bb5638315f8e21e71e
sanderdrummer <tobias.pickel@moia.io>
2023-05-23 14:19:08 +0200

    feat: enable from the start

--- plugins/user.lua
+++ plugins/user.lua
@@ -14,12 +14,12 @@ return {
     "ThePrimeagen/harpoon",
     config = function()
       require("harpoon").setup {
-        save_on_toggle = false,             -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
-        save_on_change = true,              -- saves the harpoon file upon every change. disabling is unrecommended.
-        enter_on_sendcmd = false,           -- sets harpoon to run the command immediately as it's passed to the terminal when calling `sendCommand`.
-        tmux_autoclose_windows = false,     -- closes any tmux windows harpoon that harpoon creates when you close Neovim.
+        save_on_toggle = false, -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
+        save_on_change = true, -- saves the harpoon file upon every change. disabling is unrecommended.
+        enter_on_sendcmd = false, -- sets harpoon to run the command immediately as it's passed to the terminal when calling `sendCommand`.
+        tmux_autoclose_windows = false, -- closes any tmux windows harpoon that harpoon creates when you close Neovim.
         excluded_filetypes = { "harpoon" }, -- filetypes that you want to prevent from adding to the harpoon list menu.
-        mark_branch = false,                -- set marks specific to each git branch inside git repository
+        mark_branch = false, -- set marks specific to each git branch inside git repository
         menu = {
           width = vim.api.nvim_win_get_width(0) - 4,
         },
@@ -33,9 +33,7 @@ return {
   "MunifTanjim/nui.nvim",
   {
     "AckslD/nvim-neoclip.lua",
-    requires = {
-      { "ibhagwan/fzf-lua" },
-    },
+    lazy = false,
     config = function() require("neoclip").setup() end,
   },
 }
