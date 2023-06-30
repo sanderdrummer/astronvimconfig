876eb7becb309c6d9d9381e1010990837f66df20
sanderdrummer <tobias.pickel@moia.io>
2023-06-30 14:32:18 +0200

    feat: theme updates

--- mappings.lua
+++ mappings.lua
@@ -6,6 +6,7 @@
 return {
   -- first key is the mode
   n = {
+    ["q"] = { "<Esc>" },
     ["x"] = { '"_x' },
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
--- plugins/community.lua
+++ plugins/community.lua
@@ -1,7 +1,7 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
+  -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.pack.typescript" },
@@ -10,5 +10,12 @@ return {
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.motion.mini-surround" },
+  { import = "astrocommunity.colorscheme.nord-nvim" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  { import = "astrocommunity.colorscheme.nightfox-nvim" },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.color.modes-nvim" },
+  { import = "astrocommunity.utility.neodim" },
+  { import = "astrocommunity.scrolling.vim-smoothie" },
 }
 -- { import = "astrocommunity.completion.copilot-lua-cmp" },
