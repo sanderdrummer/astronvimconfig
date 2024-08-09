3659cfe4495c2cefbb0dd8edbe2e5f80583915e4
tobiaspickel <tobias.pickel@thomann.io>
2024-08-09 11:10:39 +0200

    chore: update for nvim 10

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -30,8 +30,7 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "prettier",
         "stylua",
-        "codespell",
-        "misspell",
+        -- "codespell",
         "cspell",
         -- add more arguments for adding more null-ls sources
       })
