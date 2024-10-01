d588d93a413dde49cc175961d0b93d3ebb66d186
tobiaspickel <tobias.pickel@thomann.io>
2024-10-01 14:22:32 +0200

    chore: update settings

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -12,7 +12,6 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "lua_ls",
-        "tsserver",
         "eslint",
         "cssls",
         "cssmodules_ls",
