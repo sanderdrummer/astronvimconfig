9445c1551623dcc1f053c11a9adb064a025e9bfb
tobiaspickel <tobias.pickel@thomann.io>
2024-11-28 10:57:58 +0100

    chore: theme and lazy cleanup

--- init.lua
+++ init.lua
@@ -1,27 +1,5 @@
 require("sander.remap")
 require("config.lazy")
 
--- Setup lazy.nvim
-require("lazy").setup({
-    spec = {
-        -- import your plugins
-        { import = "plugins" },
-      },
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function () 
-          local configs = require("nvim-treesitter.configs")
-    
-          configs.setup({
-              ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-              sync_install = false,
-              highlight = { enable = true },
-              indent = { enable = true },  
-            })
-        end 
-    },
-})
+
+vim.cmd.colorscheme "catppuccin"
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,3 +14,11 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   end
 end
 vim.opt.rtp:prepend(lazypath)
+
+-- Setup lazy.nvim
+require("lazy").setup({
+    spec = {
+        -- import your plugins
+        { import = "plugins" },
+      }
+})
